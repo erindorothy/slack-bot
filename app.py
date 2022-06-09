e456ace8770e514cfa3011625cd902d5fdf0f92a
Erin Cole <erin@erins-mbp-2.attlocal.net>
2022-06-08 18:49:37 -0700

    Removing assert statement

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import gitlab
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 gl = gitlab.Gitlab(private_token=os.environ.get("GITLAB_TOKEN"))
-assert len(os.environ.get("GITLAB_TOKEN")) > 0
 # Listens to incoming messages that contain "list project issues"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
